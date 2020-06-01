97ff6e4fcfd65041075c78165d73261553faa48d
Fedor Nezhivoi <gyzerok@users.noreply.github.com>
2020-06-02 09:50:42 +1000

    add umd build of iconv-lite

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+// VSCode does not support this encodings, so we don't need to bundle them.
+// This saves us some bytes, which is important for vscode-web.
+const removeUnsupportedEncodings = [
+  "utf7.js",
+  "utf32.js",
+  "tables/cp949.json",
+].map((file) => {
+  return {
+    test: path.resolve(__dirname, "node_modules/iconv-lite/encodings", file),
+    use: "fake-loader",
+  };
+});
+
+module.exports = {
+  entry: "iconv-lite",
+  devtool: "inline-source-map",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: "iconv-lite-umd.js",
+    libraryTarget: "umd",
+    globalObject: "typeof self !== 'undefined' ? self : this",
+  },
+  module: {
+    rules: removeUnsupportedEncodings,
+  },
+  resolveLoader: {
+    alias: {
+      "fake-loader": path.resolve(__dirname, "fake-loader.js"),
+    },
+  },
+};
